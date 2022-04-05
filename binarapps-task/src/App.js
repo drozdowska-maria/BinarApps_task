01a44ad68a8d5850039982c230ba5296ea87caa5
Maria Drozdowska <drozdowska.maria1@gmail.com>
2022-04-05 21:50:22 +0200

    setting firebase and getting data

--- binarapps-task/src/App.js
+++ binarapps-task/src/App.js
@@ -1,6 +1,16 @@
-
+import { useEffect, useState } from "react";
+import { getGameData } from "./db";
 
 function App() {
+  const [gameList, setGameList] = useState([])
+  
+  useEffect(() => {
+   getGameData(setGameList)
+  }, [])
+
+  useEffect(() => {
+    console.log(gameList)
+  }, [gameList])
   return (
     <h1>
      Wordcloud game
---
+++ binarapps-task/src/db.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyClee57-ePzmUkiwPWvYXPaB9Gj-h4W-Fo",
+  authDomain: "binarapps-task.firebaseapp.com",
+  projectId: "binarapps-task",
+  storageBucket: "binarapps-task.appspot.com",
+  messagingSenderId: "1001657499385",
+  appId: "1:1001657499385:web:39a41288f5ddc7d7d12c28"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const getGameData = async (callback) => {
+    const gameDataSnapshot = await getDocs(collection(db, "gameData"))
+    const gameList = gameDataSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        question: doc.data().question,
+        allWords: doc.data().all_words,
+        goodWords: doc.data().good_words,
+    }))
+    callback(gameList)
+}
+
+export { getGameData }
